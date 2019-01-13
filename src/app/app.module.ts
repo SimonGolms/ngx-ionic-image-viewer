e3b60d628fa15f2db19571f39651ba1498914c9f
Simon G <development@gol.ms>
2019-01-13 15:59:41 +0100

    generating image-viewer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ImageViewerComponent } from './component/image-viewer/image-viewer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ImageViewerComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
---
+++ src/app/component/image-viewer/image-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-viewer',
+  templateUrl: './image-viewer.component.html',
+  styleUrls: ['./image-viewer.component.scss']
+})
+export class ImageViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
