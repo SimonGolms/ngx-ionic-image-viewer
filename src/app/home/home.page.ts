607bddf723011b3e020ba78fb00c2755ab19615c
Simon G <development@gol.ms>
2019-01-13 16:19:08 +0100

    update home page to show image viewer component on click

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ImageViewerComponent } from '../component/image-viewer/image-viewer.component';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +11,17 @@ export class HomePage {
   // Get a random image
   imgSource = 'https://picsum.photos/1000/1500/?random';
 
-  constructor() {}
+  constructor(public modalController: ModalController) {}
+
+  async viewImage(src: string) {
+    console.log('HomePage viewImage()', src);
+    const modal = await this.modalController.create({
+      component: ImageViewerComponent,
+      componentProps: { imgSource: src },
+      // cssClass: 'modal-fullscreen',
+      keyboardClose: true,
+      showBackdrop: true
+    });
+    return await modal.present();
+  }
 }
