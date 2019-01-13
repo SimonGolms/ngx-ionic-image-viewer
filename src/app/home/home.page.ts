84e19c3baebf9cadf9b1b328ed95c5eb2e45ed31
Simon G <development@gol.ms>
2019-01-13 15:58:23 +0100

    customize home page and adding img

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  // Get a random image
+  imgSource = 'https://picsum.photos/1000/1500/?random';
 
+  constructor() {}
 }
